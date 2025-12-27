d80615340035a1ee64806b535e78a1a9c9d7f1a0
Shiven Chandra <137705277+shivenchandra@users.noreply.github.com>
2025-12-27 20:01:58 +0530

    complete the home page

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,6 +1,28 @@
+import Image from "next/image" 
+import Link from "next/link"
+
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <section className="cta-section">
+      <div className="cta-badge">
+        Start learning your way.
+      </div>
+      <h2 className="text-3xl font-bold">
+        Build and Personalise Learning Companion
+      </h2>
+      <p>
+        Pick a name, subject, voice & personality and start learning through voice convos that feel natural and fun
+      </p>
+      <Image src="images/cta.svg" alt="cta" width={362} height={232} />
+      <button className="btn-primary">
+        <Image src="/icons/plus.svg" alt="plus" width={12} height={12} />
+        <Link href="/companions/new">
+          <p>
+            Create New Companion
+          </p>
+        </Link>
+      </button>
+    </section>
   )
 }
 
