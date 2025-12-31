bb0e64633ae4fd0c54a939abcea7f671f0a17df1
Shiven Chandra <137705277+shivenchandra@users.noreply.github.com>
2025-12-31 20:34:41 +0530

    implement journey/profile page

--- components/CompanionList.tsx
+++ components/CompanionList.tsx
@@ -21,7 +21,7 @@ interface CompanionListsProps {
 const CompanionList = ({ title, companions, classNames }: CompanionListsProps) => {
   return (
     <article className={cn('companion-list', classNames)}>
-      <h2 className="text-3xl font-bold">Recent Sessions</h2>
+      <h2 className="text-3xl font-bold">{title}</h2>
 
       <Table>
         <TableHeader>
